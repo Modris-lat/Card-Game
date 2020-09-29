444c3238fe597a78f7debeafc9eba91c947654c3
Modris Belkovskis <belkovskis.modris@gmail.com>
2020-09-29 13:24:23 +0300

    Exercise1 unit test project base

---
+++ Texas-holdem-engine-01/UnitTestProject/InputTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Texas_holdem_engine_01;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class InputTest
+    {
+        private IGetInput firstClass;
+
+        public InputTest()
+        {
+            firstClass = new Input();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
