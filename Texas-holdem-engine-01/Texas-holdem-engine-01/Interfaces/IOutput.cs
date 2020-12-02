6e184efa74799f385db266d587bfd0f3fe2ec7a9
Modris-lat <belkovskis.modris@gmail.com>
2020-12-02 17:10:20 +0200

    removed unused folders

--- Texas-holdem-engine-01/Texas-holdem-engine-01/Interfaces/IOutput.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Texas_holdem_engine_01.Interfaces
-{
-    public interface IOutput
-    {
-        string Output();
-        void GetOutput();
-    }
-}
--- Texas-holdem-engine-01/Texas-holdem-engine-01/StaticGetValues/CheckFlush/ReturnCheckFlushResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Texas_holdem_engine_01.StaticGetValues.CheckFlush
-{
-    public class ReturnCheckFlushResult
-    {
-        public bool Flush { get; set; }
-        public char FlushSuit { get; set; }
-    }
-}
